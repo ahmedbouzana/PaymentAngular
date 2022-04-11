5913e49bd369f4ee9df0bfc5b52c24ac6cdce0c7
ahmedbouzana <ah.bouzana@gmail.com>
2022-04-11 01:21:33 +0100

    updates to deploy to heroku7

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/PAYMENTANGULAR'));
+app.use(express.static(__dirname + '/dist/payment-angular'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/PAYMENTANGULAR/index.html'));});
+'/dist/payment-angular/index.html'));});
 app.listen(process.env.PORT || 8080);
